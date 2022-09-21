545bc3eff14ced35c5252410342d5b85a03934c0
Kirti Singh <kirtisingh1497@gmail.com>
2022-09-21 13:51:13 +0530

    fix:handled api error with catch

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-var input=document.querySelector(".input");
-var output=document.querySelector(".output");
-var button=document.querySelector(".button");
+var input = document.querySelector(".input");
+var output = document.querySelector(".output");
+var button = document.querySelector(".button");
 
 console.log(input);
 console.log(output);
 console.log(button);
 
-var apiURL="https://api.funtranslations.com/translate/ferb-latin.json"
+var apiURL = "https://api.funtranslations.com/translate/ferb-latin.jso";
 
-function constructURL(text){
-    return apiURL+"?text="+text;
+function constructURL(text) {
+  return apiURL + "?text=" + text;
 }
 
-function clickHandler(){
-    var inputValue=input.value;
-    console.log(inputValue);
-    fetch(constructURL(inputValue))
-    .then(response => response.json())
-    .then(data =>{
-        if(data.success){
-        output.classList.remove('output-error');
+function errorHandler(error) {
+  console.log("error" + error);
+  alert('Somenthing Went Wrong! Try After Sometime')
+}
+
+function clickHandler() {
+  var inputValue = input.value;
+  console.log(inputValue);
+  fetch(constructURL(inputValue))
+    .then((response) => response.json())
+    .then((data) => {
+        output.classList.remove("output-error");
         var outputResponse = data.contents.translated;
         output.innerText = outputResponse;
-        }
-        if(data.error){
-            output.classList.add('output-error')
-            var outputResponse = data.error.message;
-            output.innerText = outputResponse;
-        }
-    });
+    })
+    .catch(errorHandler);
 }
 
-button.addEventListener('click',clickHandler)
+button.addEventListener("click", clickHandler);
